697c8b07ba862fbd5a997aa38c51c9f37f2bcb3c
James Zhang <liulfbaby@gmail.com>
2020-10-13 21:03:03 +0800

    socket test not working

--- src/components/TodoList.spec.js
+++ src/components/TodoList.spec.js
@@ -1,9 +1,6 @@
 import React from 'react';
-//import Enzyme, { shallow } from "enzyme";
-//import Adapter from 'enzyme-adapter-react-16';
 import TodoList from "./TodoList";
 
-//Enzyme.configure({ adapter: new Adapter() });
 
 describe("TodoList", function(){
   it("can render todo list", function(){
---
+++ src/containers/visibleTodoList.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+describe('Visible TodoList connected component', () => {
+  it("can be render with a list", function(){
+    expect(1).to.equal(1)
+/*    let spy = sinon.spy()*/
+
+    //const visibleTodoList = mount(<VisibleTodoList />)
+
+    /*expect(visibleTodoList.find('li').length).to.equal(3)*/
+  })
+});

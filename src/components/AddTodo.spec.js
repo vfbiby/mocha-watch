de3b67c4dc2d1e76f376dc563b177988dbd28871
James Zhang <liulfbaby@gmail.com>
2020-10-19 00:53:17 +0800

    Simulate a submit with a inputed value when add a todo

--- src/components/AddTodo.spec.js
+++ src/components/AddTodo.spec.js
@@ -5,10 +5,18 @@ import {mount} from 'enzyme';
 describe('Add Todo', function() {
 
   it("can render a input and search button", function(){
-    const wrapper = mount(<AddTodo />)
+    let cb = sinon.spy()
 
+    const wrapper = mount(<AddTodo onSubmit={cb}/>)
+
+    expect(cb).to.have.been.callCount(0)
     expect(wrapper.find('input[type="text"]').length).to.equal(1)
     expect(wrapper.find('input[type="submit"]').length).to.equal(1)
+
+    wrapper.find('input[type="text"]').instance().value = 'james'
+    wrapper.find('input[type="submit"]').simulate('submit')
+    expect(cb).to.have.been.callCount(1)
+    expect(cb).to.have.been.calledWith('james')
   })
 
 });

b5b294ff1cfe9e78ea2be929c4abc50ab12a4529
Wilson Tian <wilson.tian@perkinelmer.com>
2020-10-20 13:44:28 +0800

    combine chai expect and jest expect

--- _mocha_/helpers.js
+++ _mocha_/helpers.js
@@ -1,18 +1,34 @@
 import chai from 'chai'
+import jestExpect from 'expect'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import mockCssModules from 'mock-css-modules'
 import Adapter from 'enzyme-adapter-react-16'
 import { mount, render, shallow, configure } from 'enzyme'
-
-chai.use(sinonChai)
+import * as matchers from '@testing-library/jest-dom'
 
 configure({ adapter: new Adapter() })
 
 mockCssModules.register(['.less'])
 
+chai.use(sinonChai)
+jestExpect.extend(matchers)
+
+global.expect = function expect(params) {
+  const _chaiExpect = chai.expect(params)
+  const _jestExpect = jestExpect(params)
+  return new Proxy(_chaiExpect, {
+    get: function (target, prop, receiver) {
+      try {
+        return _chaiExpect[prop]
+      } catch (error) {
+        return _jestExpect[prop]
+      }
+    }
+  })
+}
+
 global.sinon = sinon
-global.expect = chai.expect
 
 global.mount = mount
 global.render = render

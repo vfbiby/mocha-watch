a97c9b921f265ef817e2df1aead094504cd25b21
James Zhang <liulfbaby@gmail.com>
2020-10-20 23:57:52 +0800

    Add a e2e test to verify a todo can be complete by clicking

--- cypress/integration/apps/First.spec.js
+++ cypress/integration/apps/First.spec.js
@@ -2,7 +2,6 @@ describe("Todo List", function(){
   it("can list all todos", function(){
     cy.visit('http://localhost:3000')
     cy.contains('Todo')
-    cy.contains('Use Redux')
   })
 
   it("can add a todo", function(){
@@ -10,5 +9,13 @@ describe("Todo List", function(){
     cy.get('input[type=text]').type('Washing my clothes')
     cy.contains('Add Todo').click()
     cy.contains('Washing my clothes')
+  })
+
+  it("can complete a todo", function(){
+    cy.visit('http://localhost:3000')
+    cy.get('input[type=text]').type('Washing my clothes')
+    cy.contains('Add Todo').click()
+    cy.contains('Washing my clothes').click()
+    cy.contains('Washing my clothes').should('have.css', 'text-decoration', 'line-through')
   })
 })

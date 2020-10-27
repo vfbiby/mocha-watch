4e9569ba2a64d3bef5bc7a4f047d4907ecb753dd
James Zhang <liulfbaby@gmail.com>
2020-10-28 00:51:56 +0800

    Refactor index page to use redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import { createStore } from 'redux';
+import todoApp from './reducers';
+
+let store = createStore(todoApp);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
